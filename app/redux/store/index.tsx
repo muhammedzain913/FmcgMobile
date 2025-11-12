b47d28e8dbaefde71f5c19fc5455a9d4c510ea68
muhammedzain913 <zainmuhammed409@gmail.com>
2025-11-12 13:26:56 +0530

    export AppDispatch from store

--- app/redux/store/index.tsx
+++ app/redux/store/index.tsx
@@ -1,7 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import rootReducer from "../reducer";
+import { useDispatch } from "react-redux";
 
 const persistConfig = {
   key: "root",
@@ -19,6 +29,9 @@ const store = configureStore({
     }),
 });
 
- const persistor = persistStore(store);
+const persistor = persistStore(store);
 
-export  {store,persistor};
+export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
+
+export { store, persistor };
