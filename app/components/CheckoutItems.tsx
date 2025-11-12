651c2231d61dd693a57d076dd551cca4bc6a1fa9
muhammedzain913 <zainmuhammed409@gmail.com>
2025-11-12 13:21:30 +0530

    change cart item quantity  from state

--- app/components/CheckoutItems.tsx
+++ app/components/CheckoutItems.tsx
@@ -1,52 +1,69 @@
-import React ,{useState} from 'react'
-import { View, Text ,ScrollView, Image, TouchableOpacity ,} from 'react-native'
-import { useTheme } from '@react-navigation/native';
-import { Feather } from '@expo/vector-icons';
-import { COLORS,FONTS, SIZES } from '../constants/theme';
+import React, { useState } from "react";
+import { View, Text, ScrollView, Image, TouchableOpacity } from "react-native";
+import { useTheme } from "@react-navigation/native";
+import { Feather } from "@expo/vector-icons";
+import { COLORS, FONTS, SIZES } from "../constants/theme";
+import { useDispatch } from "react-redux";
+import { decrementQuantity, incrementQuantity } from "../redux/reducer/cartReducer";
 
-const CheckoutItems = () => {
+type Props = {
+  quantity: number;
+  productId : string
+};
 
-    const theme = useTheme();
-    const { colors } : {colors : any} = theme;
+const CheckoutItems = ({ quantity,productId }: Props) => {
+  const theme = useTheme();
+  const { colors }: { colors: any } = theme;
+  const dispatch = useDispatch();
 
-    const [itemQuantity, setItemQuantity] = useState(14);
+  const [itemQuantity, setItemQuantity] = useState(20);
 
   return (
     <View
-        style={{
-            flexDirection: 'row',
-            alignItems: 'center',
-        }}
+      style={{
+        flexDirection: "row",
+        alignItems: "center",
+      }}
     >
-        <TouchableOpacity
-            onPress={() => itemQuantity > 1 && setItemQuantity(itemQuantity - 1)}
-            style={{
-                height: 30,
-                width: 30,
-                backgroundColor:theme.dark ? 'rgba(255,255,255,0.10)':'#DCDCDC',
-                borderRadius: 8,
-                alignItems: 'center',
-                justifyContent: 'center',
-            }}
-        >
-            <Feather size={16} color={colors.title} name='minus' />
-        </TouchableOpacity>
-        <Text style={{ ...FONTS.fontMedium, fontSize: 14, color: colors.title, width: 40, textAlign: 'center' }}>{itemQuantity}</Text>
-        <TouchableOpacity
-            onPress={() => setItemQuantity(itemQuantity + 1)}
-            style={{
-                height: 30,
-                width: 30,
-                backgroundColor:theme.dark ? 'rgba(255,255,255,0.10)':'#DCDCDC',
-                borderRadius: 8,
-                alignItems: 'center',
-                justifyContent: 'center',
-            }}
-        >
-            <Feather size={16} color={colors.title} name='plus' />
-        </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => dispatch(decrementQuantity({id : productId}))}
+        style={{
+          height: 30,
+          width: 30,
+          backgroundColor: theme.dark ? "rgba(255,255,255,0.10)" : "#DCDCDC",
+          borderRadius: 8,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Feather size={16} color={colors.title} name="minus" />
+      </TouchableOpacity>
+      <Text
+        style={{
+          ...FONTS.fontMedium,
+          fontSize: 14,
+          color: colors.title,
+          width: 40,
+          textAlign: "center",
+        }}
+      >
+        {quantity}
+      </Text>
+      <TouchableOpacity
+        onPress={() => dispatch(incrementQuantity({id : productId}))}
+        style={{
+          height: 30,
+          width: 30,
+          backgroundColor: theme.dark ? "rgba(255,255,255,0.10)" : "#DCDCDC",
+          borderRadius: 8,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Feather size={16} color={colors.title} name="plus" />
+      </TouchableOpacity>
     </View>
-  )
-}
+  );
+};
 
-export default CheckoutItems
+export default CheckoutItems;
